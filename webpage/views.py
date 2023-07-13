55b9d45576f7d5ca851b128b75c6e93fe6221adb
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-07-13 10:47:56 +0700

    completed add new app

--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("This is my django website")
+
+
+def about(request):
+    return HttpResponse("About me")
+
+
+def contact(request):
+    return HttpResponse("Contact me")
