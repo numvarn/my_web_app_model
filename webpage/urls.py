16d396aaffca854a3fe0ffcf2cba29e84d64c18e
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-08-03 11:57:23 +0700

    take a break

--- webpage/urls.py
+++ webpage/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
+    path('details/<int:id>', views.studentDetails, name='details'),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from . import models
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'index.html')
+    context = {}
+
+    students = models.Student.objects.all().order_by("-id")
+
+    context['students'] = students
+
+    return render(request, 'index.html', context)
 
 
 def about(request):
@@ -13,3 +20,10 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def studentDetails(request, id):
+    context = {}
+    student = models.Student.objects.get(id=id)
+    context['student'] = student
+    return render(request, 'details.html', context)
